651101d28f80a42f0d9ba9fd76be3ab0390fbd4b
YUMAN1237 <69244010+yuman1237@users.noreply.github.com>
2021-06-03 08:31:14 +0530

    packages-done

--- backend/server.js
+++ backend/server.js
@@ -11,3 +11,11 @@ app.use("/api/products", productRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
+// "start": "node backend/server.js",
+
+//   "test": "echo \"Error: no test specified\" && exit 1",
+
+//     "server": "nodemon backend/server.js",
+//     "client": "npm start --prefix frontend",
+//     "start": "concurrently \"npm run server\" \"npm run client\"",
+//     "data:import": "node backend/seederScript.js",
